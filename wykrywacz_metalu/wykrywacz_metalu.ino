c95551f2f7e3e3642040c80f1aadf74ca507280d
k0lcz4tk4 <karolbaran0@gmail.com>
2018-01-06 11:45:55 +0100

    Add files via upload

---
+++ wykrywacz_metalu/wykrywacz_metalu.ino
@@ -0,0 +1,34 @@
+#define WY 2
+#define WE 8
+#define POT A1
+#define BUZZ 9
+
+void setup() {
+  pinMode(WY,OUTPUT);
+  pinMode(WE,INPUT_PULLUP);
+  pinMode(POT,INPUT);
+  pinMode(BUZZ,OUTPUT);
+}
+
+void loop() {
+  int p=0;
+  for(int i=0;i<10;i++)
+  {
+    digitalWrite(WY,1);
+    delay(5);
+    digitalWrite(WY,0);
+    delay(1);
+    int f = pulseIn(WE,1,5000);
+    int x = analogRead(POT);
+    int y = map(x,0,1023,80,120);
+    if(f>y)
+      p++;
+  }
+  if(p>=8)
+  {
+    digitalWrite(BUZZ,1);
+    delay(200);
+    digitalWrite(BUZZ,0);
+  }
+  delay(1);
+}
